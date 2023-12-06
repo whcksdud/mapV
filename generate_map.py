8e36edcd3867061fcf04b4ee31a2abc9b2650c58
whcksdud <whcksdudrns@naver.com>
2023-12-07 02:58:40 +0900

    uuid 추가

--- generate_map.py
+++ generate_map.py
@@ -4,25 +4,35 @@ import pandas as pd
 import folium
 from folium.plugins import HeatMap
 from folium import CircleMarker, Popup
+import uuid  
 
-json_file_path = sys.argv[1]
-selected_columns = sys.argv[2].split(',')
+jsondata = sys.argv[1]
+selected_columns = sys.argv[1].split(',')
 
-data = pd.read_json(json_file_path, encoding='utf8')
+
+#data = pd.read_json("Data.json")
+data = pd.DataFrame(jsondata)
 
 m = folium.Map(location=[data['위도'].mean(), data['경도'].mean()], zoom_start=15)
 
 heat_data = []
-for col in selected_columns:
-    if col in data.columns:
+for col in data:
+    if col in selected_columns:
         heat_data += [[row['위도'], row['경도'], row[col]] for index, row in data.iterrows()]
 
 HeatMap(heat_data, radius=15).add_to(m)
 
 for index, row in data.iterrows():
-    popup_text = ', '.join([f"{col}: {row[col]}" for col in selected_columns if col in row and row[col]])
+    popup_text = ', '.join([f"{col}: {row[col]}" for col in data if col in selected_columns and row[selected_columns]])
 
     circle_marker = CircleMarker(location=[row['위도'], row['경도']], radius=2, color='blue', fill=True, fill_color='blue', fill_opacity=0.5, popup=Popup(popup_text, max_width=200))
     circle_marker.add_to(m)
 m.add_child(folium.LatLngPopup())
-m.save('./heatmap.html')
+
+uuid_str = str(uuid.uuid4())
+file_name = f'./{uuid_str}.html'
+m.save(file_name)
+
+
+# file_name을 표준 출력에 출력
+print(file_name)
