77a0c67f9d7d9d3272d40afef0a43651ab06fb66
whcksdud <whcksdudrns@naver.com>
2023-12-07 18:55:19 +0900

    req.body, uuid 추가

--- generate_map.py
+++ generate_map.py
@@ -4,34 +4,36 @@ import pandas as pd
 import folium
 from folium.plugins import HeatMap
 from folium import CircleMarker, Popup
+from folium.plugins import MarkerCluster
 import uuid  
+import json
 
 jsondata = sys.argv[1]
-selected_columns = sys.argv[1].split(',')
+data = json.loads(jsondata)
+df = pd.DataFrame(data)
 
+m = folium.Map(location=[df['위도'].mean(), df['경도'].mean()], zoom_start=15)
 
-#data = pd.read_json("Data.json")
-data = pd.DataFrame(jsondata)
+# 히트맵을 위한 데이터 리스트 생성
+heat_data = [[row['위도'], row['경도']] for idx, row in df.iterrows()]
 
-m = folium.Map(location=[data['위도'].mean(), data['경도'].mean()], zoom_start=15)
+# 히트맵 생성 및 지도에 추가
+HeatMap(heat_data).add_to(m)
 
-heat_data = []
-for col in data:
-    if col in selected_columns:
-        heat_data += [[row['위도'], row['경도'], row[col]] for index, row in data.iterrows()]
+# 마커 클러스터 생성
+marker_cluster = MarkerCluster().add_to(m)
 
-HeatMap(heat_data, radius=15).add_to(m)
-
-for index, row in data.iterrows():
-    popup_text = ', '.join([f"{col}: {row[col]}" for col in data if col in selected_columns and row[selected_columns]])
-
-    circle_marker = CircleMarker(location=[row['위도'], row['경도']], radius=2, color='blue', fill=True, fill_color='blue', fill_opacity=0.5, popup=Popup(popup_text, max_width=200))
-    circle_marker.add_to(m)
+# 각 데이터 포인트에 대해 지도에 마커 추가
+for idx, row in df.iterrows():
+    folium.Marker(
+        location=[row['위도'], row['경도']],
+        popup=f"<b>{row['Name']}</b><br>Cd: {row['Cd']}, Cu: {row['Cu']}, As: {row['As']}, Pb: {row['Pb']}, Zn: {row['Zn']}, Ni: {row['Ni']}",
+        icon=folium.Icon(color='blue', icon='info-sign')
+    ).add_to(marker_cluster)
 m.add_child(folium.LatLngPopup())
 
 uuid_str = str(uuid.uuid4())
 file_name = f'./{uuid_str}.html'
 m.save(file_name)
-
 
 print(file_name)
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 8000;
 const multer  = require('multer');
 const { PythonShell } = require('python-shell');
- 
+const bodyParser = require('body-parser');
 const upload = multer({ dest: 'uploads/' });
 
 
@@ -17,12 +17,26 @@ app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public/js'));
 app.use(express.static(__dirname + '/public/css'));
 
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.post('/json', upload.single('jsonfile'), (req, res) => {
+app.post('/mapv2', (req, res) => {
+  const postData = req.body;
+
+  const options = {
+    args: [JSON.stringify(postData)] 
+  };
+  
+  PythonShell.run('generate_map.py', options, function (err, result) {
+    if (err) throw err;
+    res.json({ success: true });
+  });
+});
+
+app.post('/mapv1', upload.single('jsonfile'), (req, res) => {
   const options = {
     args: [req.file.path, req.body.selectedColumns],
   };
